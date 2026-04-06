5ad022bc9cfd83b35ee741312e54c99484b6fa3d
Ksenia Burgucheva <ksenia.burgucheva@gmail.com>
2026-04-06 09:43:03 +0200

    Add save to local storage

--- src/store.js
+++ src/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./features/todos/todoSlice";
+import { loadState, saveState } from "./localStorage";
+
+const persistedState = loadState();
 
 export const store = configureStore({
   reducer: {
     todos: todoReducer,
   },
+  preloadedState: persistedState,
+});
+
+store.subscribe(() => {
+  saveState({
+    todos: store.getState().todos,
+  });
 });
